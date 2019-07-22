f6c2543956e834c031f4eb2f2431f52690c14bcd
arvind1983 <41881552+arvind1983@users.noreply.github.com>
2019-07-22 11:38:39 +0800

    Update EMDKPowerManagerPlugin.java

--- src/android/com/dff/cordova/plugin/emdk/powermanager/EMDKPowerManagerPlugin.java
+++ src/android/com/dff/cordova/plugin/emdk/powermanager/EMDKPowerManagerPlugin.java
@@ -497,7 +497,8 @@ public class EMDKPowerManagerPlugin extends CommonPlugin implements EMDKListener
         
         if(action.equals("StartWalkMe"))
         {
-            ABBI.start("a88624d5-0689-429a-bdea-6043ee34a554", "SGlnODkvWEpqeVJMU1VNT1FJbDlrcGN4QWJoNnNKWGVWWEx6UDdob25KcWhTQ3dlNW0yZnRFclFMNE13Z3pBSzVE", ABBIFlags.ABBI_APP_HYBRID, webView.getContext());
+            ABBI.setFlag(ABBIFlags.ABBI_APP_HYBRID.getValue());
+            ABBI.start("a88624d5-0689-429a-bdea-6043ee34a554", "SGlnODkvWEpqeVJMU1VNT1FJbDlrcGN4QWJoNnNKWGVWWEx6UDdob25KcWhTQ3dlNW0yZnRFclFMNE13Z3pBSzVE", webView.getContext());
                 
             return true;
             
